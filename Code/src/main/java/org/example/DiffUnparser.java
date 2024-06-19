3b0922f8446333e75496c1dcd989c91ca7ce275a
DARIA-ACER\eshul <shulimov@mail.uni-paderborn.de>
2024-06-19 10:54:30 +0200

    in DiffUnparser unpars methode gemacht

--- Code/src/main/java/org/example/DiffUnparser.java
+++ Code/src/main/java/org/example/DiffUnparser.java
@@ -1,14 +1,18 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.Stack;
+import org.eclipse.jgit.diff.DiffAlgorithm.SupportedAlgorithm;
 import org.variantsync.diffdetective.variation.DiffLinesLabel;
 import org.variantsync.diffdetective.variation.diff.DiffNode;
+import org.variantsync.diffdetective.variation.diff.Time;
 import org.variantsync.diffdetective.variation.diff.VariationDiff;
+import org.variantsync.diffdetective.variation.diff.construction.JGitDiff;
 
 public class DiffUnparser {
-  public static String unparser(VariationDiff<DiffLinesLabel> diff){
-    StringBuilder result = new StringBuilder();
-    Stack<DiffNode<DiffLinesLabel>> stack = new Stack<>();
-    return result.toString();
+  public static String unparser(VariationDiff<DiffLinesLabel> diff) throws IOException {
+    String tree1 = TreeUnparser.unparser(diff.project(Time.BEFORE));
+    String tree2 = TreeUnparser.unparser(diff.project(Time.AFTER));
+    return JGitDiff.textDiff(tree1,tree2, SupportedAlgorithm.MYERS);
   }
 }
